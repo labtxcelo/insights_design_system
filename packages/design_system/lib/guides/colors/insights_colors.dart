086d2fe3b8484cdba45bd491f231cbe7935f4c43
Marcelo Teixeira <txcelo@gmail.com>
2023-02-26 20:50:37 -0300

    feat: creating colors and textStyles

---
+++ packages/design_system/lib/guides/colors/insights_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InsightsColors {
+  static const Color primary = Color(0xFF4c00b0);
+  static const Color secondary = Color(0xFF000000);
+  static const Color contrast = Color(0xFFFFFFFF);
+  static const Color transparent = Color(0x00000000);
+  static const Color disabled = Color(0xFFC4C4C4);
+  static const Color success = Color(0xFF198754);
+  static const Color error = Color(0xFFD0342C);
+  static const Color alert = Color(0xFFFF4F00);
+}
